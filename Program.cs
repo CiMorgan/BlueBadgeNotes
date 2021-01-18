4cc73b29a56f21262a902c09e02434b2f0e62440
Cindy Morgan <camorgan70@gmail.com>
2021-01-18 17:11:23 -0500

    commit 1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using IntroToAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = httpClient.GetAsync("https://swapi.dev/api/people/1").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                //var content = response.Content.ReadAsStringAsync().Result;  //read as string first
+                //var person = JsonConvert.DeserializeObject<Person>(content);  //and then convert to object
+
+                Person luke = response.Content.ReadAsAsync<Person>().Result;  //yield the object of the specified type automatically (1 step process)
+                Console.WriteLine(luke.Name);
+
+                foreach (string vehicleUrl in luke.Vehicles)
+                {
+                    HttpResponseMessage vehicleResponse = httpClient.GetAsync(vehicleUrl).Result;
+                    //Console.WriteLine(vehicleResponse.Content.ReadAsStringAsync().Result);   //string of Json object (not an actual object)
+
+                    Vehicle vehicle = vehicleResponse.Content.ReadAsAsync<Vehicle>().Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+            }
+
+            SWAPIService service = new SWAPIService();
+            Person person = service.GetPersonAsync("https://swapi.dev/api/people/11/").Result;
+            if (person != null)
+            {
+                Console.WriteLine(person.Name);
+
+                foreach (var vehicleUrl in person.Vehicles)
+                {
+                    var vehicle = service.GetVehicleAsync(vehicleUrl).Result;
+                    Console.WriteLine(vehicle.Name);
+                }
+            }
+        }
+    }
+}
---
+++ SWAPIService.cs
@@ -0,0 +1,36 @@
+﻿using IntroToAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToAPI
+{
+    class SWAPIService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<Person> GetPersonAsync(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)  
+            {
+                //was a success
+                Person person = await response.Content.ReadAsAsync<Person>();
+                return person;
+            }
+            return null;  //was not a success
+        }
+
+        public async Task<Vehicle> GetVehicleAsync (string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            return response.IsSuccessStatusCode  //same as if statement above
+                ? await response.Content.ReadAsAsync<Vehicle>()   //if true
+                : null;                                           //if false
+        }
+    }
+}
